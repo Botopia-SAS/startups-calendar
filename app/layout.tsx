e0048059d7cec603dd46717c08365825a19137c1
David Espejo <davas.espejo@gmail.com>
2024-12-29 21:13:49 -0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,6 @@
 import './globals.css';
 import Footer from '@/components/Footer';
 import Header from '@/components/header';
-import { Montserrat } from '@next/font/google';
-
-// Configuración de la fuente Montserrat
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400', '700'], // Pesos usados en el diseño
-});
 
 export const metadata = {
   title: 'Startups Calendar',
@@ -21,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es" className={montserrat.className}>
+    <html lang="es">
       <body className="bg-gray-50 flex flex-col h-[100dvh]">
         <Header />
         <main className="flex-grow">{children}</main>
