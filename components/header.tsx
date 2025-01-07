34ef77f91ef278568b1e3a841c340ea546b3eb36
David Espejo <davas.espejo@gmail.com>
2025-01-06 19:38:04 -0500

    add lenguajes

--- components/header.tsx
+++ components/header.tsx
@@ -43,11 +43,12 @@ const Header = () => {
             <select
               value={currentLanguage}
               onChange={(e) => changeLanguage(e.target.value)}
-              className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-800 bg-white"
             >
               <option value="es">Es</option>
               <option value="en">En</option>
             </select>
+
           </div>
 
           {/* Navigation Links */}
